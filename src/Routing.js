b35fd8a118ac4ec950d27c5b3ffaa6de20391199
Guillaume CLEMENT--BONNIEL-VEYRON <guillaume.clement-bonniel-veyron@epitech.eu>
2025-02-03 22:08:10 +0900

    turns out I didn't fix anything

--- src/Routing.js
+++ src/Routing.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Switch, Redirect, Route } from 'react-router-dom';
+import { Routes, Navigate, Route } from 'react-router-dom';
 
-const Routes = () => {
+const Routing = () => {
     return (
-        <Switch>
+        <Routes>
             {/* This endpoint will just return you to a dummy HTML with a simple heading tag */}
-            <Route path="/health">
+            <Route path='/health'>
                 <h3>Hey There!!! The App is Healthy</h3>
             </Route>
 
             {/* All other routes will be defined here */}
 
             {/* Finally you will be redirected to a not found page */}
-            <Redirect to="/not-found" />
-        </Switch>
+            <Navigate to='/not-found' />
+        </Routes>
     );
 };
 
-export default Routes;
+export default Routing;
