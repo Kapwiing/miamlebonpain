2481544812ed00287a8ab73bc0ea5d79543150ab
Guillaume CLEMENT--BONNIEL-VEYRON <guillaume.clement-bonniel-veyron@epitech.eu>
2025-02-03 15:37:15 +0900

    Yea.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import logo from './pain_logo.png';
 import './App.css';
+import Alpine from 'alpinejs'
+import { use, useEffect } from 'react';
+
+window.Alpine = Alpine
 
 function App() {
+
+  useEffect(() => {
+    Alpine.start()
+  }, [0])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Kill yourself.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
